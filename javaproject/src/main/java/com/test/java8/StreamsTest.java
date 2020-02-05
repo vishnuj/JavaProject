9a49deef1b5543189ebecd7d36eee7943f4279f0
145670 <145670@G635LLAX.ustr.com>
2020-02-05 09:58:37 -0600

    Changes

--- javaproject/src/main/java/com/test/java8/StreamsTest.java
+++ javaproject/src/main/java/com/test/java8/StreamsTest.java
@@ -1,9 +1,13 @@
 package com.test.java8;
 
+import java.util.stream.*;
+
 public class StreamsTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stream.iterate(1, a->a+1).filter(a-> a%2!=0).limit(10).forEach(a->System.out.println(a));
+		Stream.iterate(10, a->a+1).filter(a-> a%2==0).limit(10).forEach(System.out::println);
+		
 		
 	}
 
